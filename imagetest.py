69413637deb6e2a46847c851cbbc2ffb93ccef13
Kevin Claytor <ke.claytor@gmail.com>
2014-08-16 15:05:10 -0400

    Adding in test image script.

--- imagetest.py
+++ imagetest.py
@@ -5,17 +5,31 @@ import pygame
 import numpy
 import pendulo
 
+def load_stormtrooper():
+    png = pygame.image.load('stormtrooper.png')
+    png = pygame.transform.rotate(stormpng, 90)
+    arr = pygame.surfarray.array2d(stormpng)
+    whiteval = numpy.amin(arr)
+    blackval = numpy.amax(arr) 
+    return (arr, whiteval, blackval)
+
+def load_grid():
+    png = pygame.image.load('grid.png')
+    png = pygame.transform.rotate(stormpng, 90)
+    arr = pygame.surfarray.array2d(stormpng)
+    whiteval = numpy.amax(arr)
+    blackval = numpy.amin(arr) 
+    return (arr, whiteval, blackval)
+
 if __name__ == "__main__":
-    stormpng = pygame.image.load('stormtrooper.png')
-    stormarr = pygame.surfarray.array2d(stormpng)
-    whiteval = numpy.amin(stormarr)
-    blackval = numpy.amax(stormarr) 
+
+    (imagearr, whiteval, blackval) = load_grid()
 
     # Half the period of the pendulum
     #  (time for one swing from l->r or r->l, ie; one 'line')
     half_period = 2.00 / 2.0
     # How long to spend belaying the pen to the next line
-    belay_time = 0.05
+    belay_time = 0.01
     # And now send it to the write routine
-    pendulo.draw(stormarr, whiteval, blackval, half_period, belay_time)
+    pendulo.draw(imagearr, whiteval, blackval, half_period, belay_time)
 
